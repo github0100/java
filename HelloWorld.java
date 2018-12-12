8cb5813f5569adcb8a1321a0f29168d8b87c279a
root <root@Devi.localdomain>
2018-12-11 23:05:12 -0600

    ss

---
+++ HelloWorld.java
@@ -0,0 +1,52 @@
+ckage week2;
+
+public class StringDemo {
+
+		public static void main(String[] args) {
+					// TODO Auto-generated method stub
+					// 		
+					// 				String name="I am learning Java";
+					// 						
+					// 										
+					// 												System.out.println(name.length());//to get the length
+					// 														System.out.println(name.substring(5));
+					// 																System.out.println(name.substring(5,12));
+					// 																		System.out.println(name.toLowerCase());
+					// 																				System.out.println(name.toUpperCase());
+					// 																						System.out.println(name.replace('J', 'L'));
+					// 																								String replaceString=name.replace("am","this"); 
+					// 																										System.out.println(replaceString); 
+					// 																												System.out.println(name.contains("Java"));
+					// 																														
+					// 																																String s1="hello";
+					// 																																		String s2="hello"; 
+					// 																																				String s3="hemlo"; 
+					// 																																						String s4="flag";
+					// 																																								System.out.println(s1.compareTo(s2)); // 0 because both are equal
+					// 																																										System.out.println(s1.compareTo(s3));//-1 because "l" is only one time lower than "m" 
+					// 																																												System.out.println(s1.compareTo(s4)); // 2 because "h" is 2 times greater than "f"
+					// 																																														/*
+					// 																																																if s1 > s2, it returns a positive number  
+					// 																																																		if s1 < s2, it returns a negative number 
+					// 																																																				if s1 == s2, it returns 0*/
+					// 																																																						
+					// 																																																								
+					// 																																																										String s5="hello";
+					// 																																																												s5=s5.concat("how are you");
+					// 																																																														System.out.println(s5);
+					// 																																																																
+					// 																																																																		String s6="hello"; 
+					// 																																																																				String s7="hello"; 
+					// 																																																																						String s8="hi";
+					// 																																																																								System.out.println(s6.equalsIgnoreCase(s7));   // returns true
+					// 																																																																										System.out.println(s6.equalsIgnoreCase(s8));   // returns false
+					// 																																																																												
+					// 																																																																														String s9=""; 
+					// 																																																																																String s10="hello";
+					// 																																																																																		System.out.println(s9.isEmpty());     // returns true
+					// 																																																																																				System.out.println(s10.isEmpty());     // returns false
+					//
+					// 																																																																																					}
+					//
+					// 																																																																																					}
+					//
